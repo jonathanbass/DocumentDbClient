aa88db2ffc4a9d39d60e5d9a48a6940a5059fefd
Jonathan Bass <jonathan.bass@parexel.com>
2018-06-28 09:59:52 +0100

    initial commit

---
+++ DocumentDb/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Configuration;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+
+namespace DocumentDb
+{
+    internal class Program
+    {
+        private static string _documentDbEndpoint;
+        private static string _documentDbAuthKey;
+        private static string _testDatabaseName;
+
+        private static void Main()
+        {
+            _documentDbEndpoint = ConfigurationManager.AppSettings["DocumentDbEndpoint"];
+            _documentDbAuthKey = ConfigurationManager.AppSettings["DocumentDbAuthKey"];
+            _testDatabaseName = Guid.NewGuid().ToString();
+
+            CreateTestDatabase().Wait();
+            TeardownTestDatabase().Wait();
+        }
+
+        private static async Task CreateTestDatabase()
+        {
+            using (var client = new DocumentClient(new Uri(_documentDbEndpoint), _documentDbAuthKey))
+            {
+                await client.CreateDatabaseIfNotExistsAsync(new Database { Id = _testDatabaseName });
+            }
+        }
+
+        private static async Task TeardownTestDatabase()
+        {
+            using (var client = new DocumentClient(new Uri(_documentDbEndpoint), _documentDbAuthKey))
+            {
+                var db = await client.ReadDatabaseAsync(UriFactory.CreateDatabaseUri(_testDatabaseName));
+
+                await client.DeleteDatabaseAsync(UriFactory.CreateDatabaseUri(db.Resource.Id));
+            }
+        }
+    }
+}
